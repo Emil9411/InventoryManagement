8fa5b4bf7d0ed65b61182895e3a690e783548db6
Emil9411 <almadi.emil94@gmail.com>
2024-07-08 21:51:17 +0200

    Interface for handling items created.

---
+++ InventoryManagement.Server/Restaurant1/Repository/IRestaurant1RepoAllItem.cs
@@ -0,0 +1,14 @@
+﻿using InventoryManagement.Server.Model.Restaurants;
+
+namespace InventoryManagement.Server.Restaurant1.Repository
+{
+    public interface IRestaurant1RepoAllItem
+    {
+        IEnumerable<Restaurant1Item> GetAllItems();
+        IEnumerable<Restaurant1Item> GetAllMaterialItems();
+        IEnumerable<Restaurant1Item> GetAllNonMaterialItems();
+        IEnumerable<Restaurant1Item> GetAllItemsToOrder();
+        IEnumerable<Restaurant1Item> GetAllFoodOrDrinkItems();
+        IEnumerable<Restaurant1Item> GetAllNonFoodOrDrinkItems();
+    }
+}
---
+++ InventoryManagement.Server/Restaurant1/Repository/IRestaurant1RepoSingleItem.cs
@@ -0,0 +1,13 @@
+﻿using InventoryManagement.Server.Model.Restaurants;
+
+namespace InventoryManagement.Server.Restaurant1.Repository
+{
+    public interface IRestaurant1RepoSingleItem
+    {
+        Restaurant1Item? GetItemById(int id);
+        Restaurant1Item? GetItemByName(string name);
+        void CreateItem(Restaurant1Item item);
+        void UpdateItem(Restaurant1Item item);
+        void DeleteItem(int id);
+    }
+}
