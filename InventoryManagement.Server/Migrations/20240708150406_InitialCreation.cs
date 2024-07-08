d0e0281f466f2afea7eb4f10fc7e303cbb4c8be2
Emil9411 <almadi.emil94@gmail.com>
2024-07-08 17:33:15 +0200

    Database migrations created and applied.

---
+++ InventoryManagement.Server/Migrations/20240708150406_InitialCreation.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryManagement.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Restaurant3",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    QuantityUnit = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    QuantityPerPackage = table.Column<int>(type: "int", nullable: false),
+                    ShouldOrder = table.Column<bool>(type: "bit", nullable: false),
+                    IsMaterial = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurant3", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Restaurant3_Name",
+                table: "Restaurant3",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Restaurant3");
+        }
+    }
+}
---
+++ InventoryManagement.Server/Migrations/20240708152503_RepairTables3.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryManagement.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class RepairTables3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Restaurant1",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    QuantityUnit = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    QuantityPerPackage = table.Column<int>(type: "int", nullable: false),
+                    ShouldOrder = table.Column<bool>(type: "bit", nullable: false),
+                    IsMaterial = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurant1", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Restaurant2",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    QuantityUnit = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    QuantityPerPackage = table.Column<int>(type: "int", nullable: false),
+                    ShouldOrder = table.Column<bool>(type: "bit", nullable: false),
+                    IsMaterial = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurant2", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Restaurant1_Name",
+                table: "Restaurant1",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Restaurant2_Name",
+                table: "Restaurant2",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Restaurant1");
+
+            migrationBuilder.DropTable(
+                name: "Restaurant2");
+        }
+    }
+}
