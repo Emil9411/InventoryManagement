1a465cc6df6162fd286603477e9252d614d26255
Emil9411 <almadi.emil94@gmail.com>
2024-07-12 20:42:59 +0200

    Created new Restaurant1Repo class.

---
+++ InventoryManagement.Server/Restaurant1/Restaurant1Repo.cs
@@ -0,0 +1,13 @@
+﻿using InventoryManagement.Server.Context;
+using InventoryManagement.Server.Model.Restaurants;
+using InventoryManagement.Server.Restaurant1.Repository;
+
+namespace InventoryManagement.Server.Restaurant1
+{
+    public class Restaurant1Repo : GenericRepo<Restaurant1Item>
+    {
+        public Restaurant1Repo(ItemContext context, ILogger<Restaurant1Repo> logger) : base(context, logger)
+        {
+        }
+    }
+}
