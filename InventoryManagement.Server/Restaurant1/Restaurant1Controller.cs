84dd53ee2eb2f30a93200e0a8b8c600a73a75d20
Emil9411 <almadi.emil94@gmail.com>
2024-07-12 20:43:23 +0200

    Created new Restaurant1Controller class

---
+++ InventoryManagement.Server/Restaurant1/Restaurant1Controller.cs
@@ -0,0 +1,16 @@
+﻿using InventoryManagement.Server.Model.Restaurants;
+using InventoryManagement.Server.Restaurant1.Controller;
+using InventoryManagement.Server.Restaurant1.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagement.Server.Restaurant1
+{
+    [ApiController]
+    [Route("api/restaurant1/items")]
+    public class Restaurant1Controller : GenericController<Restaurant1Item>
+    {
+        public Restaurant1Controller(IGenericRepo<Restaurant1Item> genericRepo, ILogger<GenericController<Restaurant1Item>> logger) : base(genericRepo, logger)
+        {
+        }
+    }
+}
