8fdedf534bb34289856ceb57f00c9c3054999a64
Emil9411 <almadi.emil94@gmail.com>
2024-07-12 23:42:56 +0200

    AppUser class created, which inherits from IdentityUser.

---
+++ InventoryManagement.Server/Model/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace InventoryManagement.Server.Model
+{
+    public class AppUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+        public string? PostalCode { get; set; }
+    }
+}
