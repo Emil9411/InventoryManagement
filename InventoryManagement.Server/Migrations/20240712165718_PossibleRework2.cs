9068c7b5221875bfa893bdfdbfadbb57f39db2a9
Emil9411 <almadi.emil94@gmail.com>
2024-07-12 20:41:01 +0200

    Database migrations applied.

---
+++ InventoryManagement.Server/Migrations/20240712165718_PossibleRework2.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryManagement.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class PossibleRework2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Item",
+                table: "Item");
+
+            migrationBuilder.RenameTable(
+                name: "Item",
+                newName: "Restaurant3Items");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Item_Name",
+                table: "Restaurant3Items",
+                newName: "IX_Restaurant3Items_Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Restaurant3Items",
+                table: "Restaurant3Items",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Restaurant3Items",
+                table: "Restaurant3Items");
+
+            migrationBuilder.RenameTable(
+                name: "Restaurant3Items",
+                newName: "Item");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Restaurant3Items_Name",
+                table: "Item",
+                newName: "IX_Item_Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Item",
+                table: "Item",
+                column: "Id");
+        }
+    }
+}
---
+++ InventoryManagement.Server/Migrations/20240712180752_PossibleRework3.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryManagement.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class PossibleRework3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Restaurant3Items");
+
+            migrationBuilder.CreateTable(
+                name: "Restaurant1",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    QuantityUnit = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    QuantityPerPackage = table.Column<int>(type: "int", nullable: false),
+                    ShouldOrder = table.Column<bool>(type: "bit", nullable: false),
+                    IsMaterial = table.Column<bool>(type: "bit", nullable: false),
+                    IsFoodOrDrink = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurant1", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Restaurant2",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    QuantityUnit = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    QuantityPerPackage = table.Column<int>(type: "int", nullable: false),
+                    ShouldOrder = table.Column<bool>(type: "bit", nullable: false),
+                    IsMaterial = table.Column<bool>(type: "bit", nullable: false),
+                    IsFoodOrDrink = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurant2", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Restaurant3",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    QuantityUnit = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    QuantityPerPackage = table.Column<int>(type: "int", nullable: false),
+                    ShouldOrder = table.Column<bool>(type: "bit", nullable: false),
+                    IsMaterial = table.Column<bool>(type: "bit", nullable: false),
+                    IsFoodOrDrink = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurant3", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Restaurant1_Name",
+                table: "Restaurant1",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Restaurant2_Name",
+                table: "Restaurant2",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Restaurant3_Name",
+                table: "Restaurant3",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Restaurant1");
+
+            migrationBuilder.DropTable(
+                name: "Restaurant2");
+
+            migrationBuilder.DropTable(
+                name: "Restaurant3");
+
+            migrationBuilder.CreateTable(
+                name: "Restaurant3Items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsFoodOrDrink = table.Column<bool>(type: "bit", nullable: false),
+                    IsMaterial = table.Column<bool>(type: "bit", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    QuantityPerPackage = table.Column<int>(type: "int", nullable: false),
+                    QuantityUnit = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ShouldOrder = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurant3Items", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Restaurant3Items_Name",
+                table: "Restaurant3Items",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+        }
+    }
+}
