126598ecd40f3ca1e832754a711942449dcbfc8c
Emil9411 <almadi.emil94@gmail.com>
2024-07-14 12:59:11 +0200

    Registrationrequest model extended with Role attribute

--- InventoryManagement.Server/Authorization/Requests/RegistrationRequest.cs
+++ InventoryManagement.Server/Authorization/Requests/RegistrationRequest.cs
@@ -4,5 +4,6 @@ namespace InventoryManagement.Server.Authorization.Requests
 {
     public record RegistrationRequest([Required] string Email,
                                       [Required] string Username,
-                                      [Required] string Password);
+                                      [Required] string Password,
+                                      [Required] string Role);
 }
