e995ee2b391532eddeea54bdbdf43c5df6061c1a
Emil9411 <almadi.emil94@gmail.com>
2024-07-16 20:31:27 +0200

    Email verification code has model, table created.

---
+++ InventoryManagement.Server/Model/VerificationCode.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoryManagement.Server.Model
+{
+    public class VerificationCode
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Code { get; set; }
+        public DateTime ExpirationTime { get; set; }
+    }
+}
