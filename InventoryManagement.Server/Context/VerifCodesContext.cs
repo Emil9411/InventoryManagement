5f5aa00447b845b27d72d64663471ab834d6d42f
Emil9411 <almadi.emil94@gmail.com>
2024-07-16 20:32:09 +0200

    Code verification context created.

---
+++ InventoryManagement.Server/Context/VerifCodesContext.cs
@@ -0,0 +1,21 @@
+﻿using InventoryManagement.Server.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagement.Server.Context
+{
+    public class VerifCodesContext : DbContext
+    {
+        public VerifCodesContext(DbContextOptions<VerifCodesContext> options) : base(options)
+        {
+        }
+
+        public DbSet<VerificationCode> VerificationCodes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VerificationCode>()
+                .HasIndex(vc => vc.UserId)
+                .IsUnique();
+        }
+    }
+}
