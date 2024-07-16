62194fed9c2e06d5eac10326031a7d52f2cb89aa
Emil9411 <almadi.emil94@gmail.com>
2024-07-16 20:34:06 +0200

    Interface for email sending, and verifying created.

---
+++ InventoryManagement.Server/Authorization/Services/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿namespace InventoryManagement.Server.Authorization.Services
+{
+    public interface IEmailSender
+    {
+        void SendEmail(string username, string email, string subject, string body, string? verificationCode);
+        string GenerateVerificationCode();
+        void SaveVerificationCode(string email, string verificationCode);
+    }
+}
