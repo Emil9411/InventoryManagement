5920863321036d90be87dcc52630c853f4644160
Emil9411 <almadi.emil94@gmail.com>
2024-07-17 21:18:04 +0200

    Login page inserted into the react router.

--- inventorymanagement.client/src/main.jsx
+++ inventorymanagement.client/src/main.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import App from './App.jsx'
+import Login from './pages/Login.jsx'
 import './index.css'
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
-        children: [],
+        children: [
+            {
+                path: "/login",
+                element: <Login />
+            }
+        ]
     },
 ]);
 
