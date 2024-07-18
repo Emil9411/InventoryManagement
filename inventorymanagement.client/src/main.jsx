c831ffbf3e1f38c8c77d5545294af60d551e5f50
Emil9411 <almadi.emil94@gmail.com>
2024-07-18 17:31:00 +0200

    Logout route added to the router.

--- inventorymanagement.client/src/main.jsx
+++ inventorymanagement.client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import App from './App.jsx'
 import Login from './pages/Login.jsx'
+import Logout from './components/LogoutButton.jsx'
 import './index.css'
 
 const router = createBrowserRouter([
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
             {
                 path: "/login",
                 element: <Login />
+            },
+            {
+                path: "/logout",
+                element: <Logout />
             }
         ]
     },
