fd8167eacac65cf9d9efce7b4ba362aeb652466c
Emil9411 <almadi.emil94@gmail.com>
2024-07-19 08:39:57 +0200

    Link to AllItems added, footer added.

--- inventorymanagement.client/src/App.jsx
+++ inventorymanagement.client/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
             <div className="header">
                 <div className="header-title">
                     <Link to="/">
-                        <h1 style={{ color: "white" }}>Raktár Manager 2000</h1>
+                        <h1 style={{ color: "white" }}>Raktár Manager</h1>
                     </Link>
                 </div>
                 <div className="header-buttons">
@@ -41,7 +41,9 @@ function App() {
                         <>
                             <LogoutButton />
                             <button>Regisztráció</button>
-                            <button>Minden termék</button>
+                            <Link to="/allitems">
+                                <button>Minden termék</button>
+                            </Link>
                             <button>Kivét</button>
                             <button>Bevét</button>
                             <button>Termék hozzáadása</button>
@@ -49,13 +51,20 @@ function App() {
                     ) : (
                         <>
                             <LogoutButton />
-                            <button>Minden termék</button>
+                            <Link to="/allitems">
+                                <button>Minden termék</button>
+                            </Link>
                             <button>Kivét</button>
                         </>
                     )}
                 </div>
             </div>
             <Outlet />
+            <div className="footer">
+                <p>© 2024 Raktár Manager</p>
+                <p>Created by: EM&EM Software</p>
+                <p>(Under registration)</p>
+            </div>
         </div>
     );
 }
