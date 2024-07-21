cc1202bb479bb12a64bf555f4d46bc274efd44f6
Emil9411 <almadi.emil94@gmail.com>
2024-07-21 21:30:14 +0200

    New interface to handle items in the repository created.

---
+++ InventoryManagement.Server/Repository/IItemRepo.cs
@@ -0,0 +1,22 @@
+﻿using InventoryManagement.Server.Model;
+
+namespace InventoryManagement.Server.Repository
+{
+    public interface IItemRepo
+    {
+        Task<Item> GetItemByIdAsync(int id);
+        Task<IEnumerable<Item>> GetItemByNameAsync(string name, int? inventoryId);
+        Task CreateItemAsync(Item item);
+        Task UpdateItemAsync(Item item);
+        Task DeleteItemAsync(int id);
+        Task<Item> UpdateShouldOrderStatusAsync(int id, bool shouldOrder);
+        Task<IEnumerable<Item>> GetAllItemsAsync(int? inventoryId);
+        Task<IEnumerable<Item>> GetAllIngredientsAsync(int? inventoryId);
+        Task<IEnumerable<Item>> GetAllNonIngredientsAsync(int? inventoryId);
+        Task<IEnumerable<Item>> GetAllItemsToOrderAsync(int? inventoryId);
+        Task<IEnumerable<Item>> GetAllConsumableItemsAsync(int? inventoryId);
+        Task<IEnumerable<Item>> GetAllNonConsumableItemsAsync(int? inventoryId);
+        Task<IEnumerable<Item>> GetAllEquipmentItemsAsync(int? inventoryId);
+        Task<IEnumerable<Item>> GetAllNonEquipmentItemsAsync(int? inventoryId);
+    }
+}
