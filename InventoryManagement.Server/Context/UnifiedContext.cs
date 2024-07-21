cb39f2b3c30df60bea05b85132cff674b2f00919
Emil9411 <almadi.emil94@gmail.com>
2024-07-21 21:04:57 +0200

    Context modified to accommodate the new requirements.

--- InventoryManagement.Server/Context/UnifiedContext.cs
+++ InventoryManagement.Server/Context/UnifiedContext.cs
@@ -1,5 +1,4 @@
 ﻿using InventoryManagement.Server.Model;
-using InventoryManagement.Server.Model.Restaurants;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -13,33 +12,47 @@ namespace InventoryManagement.Server.Context
         }
 
         public DbSet<VerificationCode> VerificationCodes { get; set; }
-        public DbSet<Restaurant1Item> Restaurant1Items { get; set; }
-        public DbSet<Restaurant2Item> Restaurant2Items { get; set; }
-        public DbSet<Restaurant3Item> Restaurant3Items { get; set; }
+        public DbSet<Inventory> Inventories { get; set; }
+        public DbSet<Item> Items { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            // VerificationCode configuration
-            modelBuilder.Entity<VerificationCode>()
-                .HasIndex(vc => vc.UserId)
+            modelBuilder.Entity<Item>()
+                .HasOne(i => i.Inventory)
+                .WithMany(i => i.Items)
+                .HasForeignKey(i => i.InventoryId);
+            modelBuilder.Entity<Item>()
+                .HasIndex(i => new { i.Name, i.InventoryId })
                 .IsUnique();
 
-            // ItemContext configurations
-            modelBuilder.Entity<Restaurant1Item>().ToTable("Restaurant1");
-            modelBuilder.Entity<Restaurant2Item>().ToTable("Restaurant2");
-            modelBuilder.Entity<Restaurant3Item>().ToTable("Restaurant3");
+            modelBuilder.Entity<Inventory>()
+                .HasMany(i => i.Employees)
+                .WithOne(u => u.Inventory)
+                .HasForeignKey(u => u.InventoryId);
+            modelBuilder.Entity<Inventory>()
+                .HasOne(i => i.Manager)
+                .WithOne(u => u.ManagedInventory)
+                .HasForeignKey<Inventory>(i => i.ManagerId);
+            modelBuilder.Entity<Inventory>()
+                .HasMany(i => i.Items)
+                .WithOne(i => i.Inventory)
+                .HasForeignKey(i => i.InventoryId);
+            modelBuilder.Entity<Inventory>()
+                .HasIndex(i => i.Name)
+                .IsUnique();
 
-            modelBuilder.Entity<Restaurant1Item>()
-                .HasIndex(i => i.Name)
-                .IsUnique();
-            modelBuilder.Entity<Restaurant2Item>()
-                .HasIndex(i => i.Name)
-                .IsUnique();
-            modelBuilder.Entity<Restaurant3Item>()
-                .HasIndex(i => i.Name)
-                .IsUnique();
+            modelBuilder.Entity<AppUser>()
+                .HasOne(u => u.Inventory)
+                .WithMany(i => i.Employees)
+                .HasForeignKey(u => u.InventoryId)
+                .OnDelete(DeleteBehavior.NoAction);
+            modelBuilder.Entity<AppUser>()
+                .HasOne(u => u.ManagedInventory)
+                .WithOne(i => i.Manager)
+                .HasForeignKey<AppUser>(u => u.ManagedInventoryId)
+                .OnDelete(DeleteBehavior.NoAction);
         }
     }
 }
