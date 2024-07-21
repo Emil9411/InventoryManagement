72974b5e84aa167d7e448080da155f1349c867dc
Emil9411 <almadi.emil94@gmail.com>
2024-07-21 20:59:03 +0200

    New model class for storing inventories.

---
+++ InventoryManagement.Server/Model/Inventory.cs
@@ -0,0 +1,18 @@
+﻿namespace InventoryManagement.Server.Model
+{
+    public class Inventory
+    {
+        public int InventoryId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string ManagerId { get; set; }
+        public AppUser Manager { get; set; }
+        public ICollection<AppUser>? Employees { get; set; }
+        public ICollection<Item>? Items { get; set; }
+    }
+}
