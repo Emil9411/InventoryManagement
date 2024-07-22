409818a393ab6a9af9517ab9ed63bd0f997f2195
Emil9411 <almadi.emil94@gmail.com>
2024-07-22 20:42:45 +0200

    Item model modified.

--- InventoryManagement.Server/Model/Item.cs
+++ InventoryManagement.Server/Model/Item.cs
@@ -4,9 +4,19 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Quantity { get; set; }
-        public string QuantityUnit { get; set; }
+        public int WeightInGrams { get; set; }
+        public double WeightInKilograms { get; set; }
+        public int QuantityInPieces { get; set; }
         public int QuantityPerPackage { get; set; }
+        public double NetPricePerGram { get; set; }
+        public double NetPricePerKilogram { get; set; }
+        public double NetPricePerPiece { get; set; }
+        public double NetPricePerPackage { get; set; }
+        public double TaxRate { get; set; }
+        public double GrossPricePerGram { get; set; }
+        public double GrossPricePerKilogram { get; set; }
+        public double GrossPricePerPiece { get; set; }
+        public double GrossPricePerPackage { get; set; }
         public bool ShouldOrder { get; set; }
         public bool IsIngredient { get; set; }
         public bool IsConsumable { get; set; }
