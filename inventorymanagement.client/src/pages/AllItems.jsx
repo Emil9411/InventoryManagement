05a2dcec19d5f1a7713156c9d72ed328009c510e
Emil9411 <almadi.emil94@gmail.com>
2024-07-24 22:06:46 +0200

    AllItems page functionally created, item presentation not made yet.

--- inventorymanagement.client/src/pages/AllItems.jsx
+++ inventorymanagement.client/src/pages/AllItems.jsx
@@ -1,7 +1,46 @@
+import { useState, useEffect } from 'react';
+import "../index.css";
+
 function AllItems() {
-  return (
-    <p>Hello world!</p>
-  );
+    const [items, setItems] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    async function getItems() {
+        try {
+            const response = await fetch("api/item/getAllItems", {
+                method: "GET",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+                setItems(data);
+            }
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        getItems();
+    }, []);
+
+    if (loading) {
+        return (
+            <>
+                <br />
+                <div className="spinner"></div>
+            </>
+        );
+    }
+    return (
+        <p>Hello world!</p>
+    );
 }
 
 export default AllItems;
