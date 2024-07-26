946e03ab91b89aba6824d2c66acdf33a1e543799
Emil9411 <almadi.emil94@gmail.com>
2024-07-27 01:29:02 +0200

    User data transfer object created.

---
+++ InventoryManagement.Server/Authorization/Responses/UserDto.cs
@@ -0,0 +1,14 @@
+﻿namespace InventoryManagement.Server.Authorization.Responses
+{
+    public record UserDto(string Id,
+                          string LastName,
+                          string FirstName,
+                          string City,
+                          string PostalCode,
+                          string Address,
+                          string Email,
+                          string PhoneNumber,
+                          string UserName,
+                          string Role,
+                          bool EmailConfirmed);
+}
