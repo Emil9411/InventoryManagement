9e46e388bc8fcddd5263b6ef4a7e0eaeb25a6ca6
Emil9411 <almadi.emil94@gmail.com>
2024-07-29 19:59:21 +0200

    Interface for handling inventory related tasks created.

---
+++ InventoryManagement.Server/Repository/IInventoryRepo.cs
@@ -0,0 +1,18 @@
+﻿using InventoryManagement.Server.Model;
+
+namespace InventoryManagement.Server.Repository
+{
+    public interface IInventoryRepo
+    {
+        Task<Inventory> GetInventoryByIdAsync(int id);
+        Task<IEnumerable<Inventory>> GetAllInventoriesAsync();
+        Task CreateInventoryAsync(Inventory inventory);
+        Task DeleteInventoryAsync(int id);
+        Task UpdateInventoryAsync(Inventory inventory);
+        Task<bool> InventoryExistsAsync(int id);
+        Task AddEmployeeToInventoryAsync(int inventoryId, int employeeId);
+        Task RemoveEmployeeFromInventoryAsync(int inventoryId, int employeeId);
+        Task<IEnumerable<AppUser>> GetEmployeesByInventoryIdAsync(int inventoryId);
+
+    }
+}
