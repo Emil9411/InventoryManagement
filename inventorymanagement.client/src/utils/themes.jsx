ca6cb4c3184f6ab9f6cdcae2edb5757716799975
Emil9411 <almadi.emil94@gmail.com>
2024-08-05 18:34:28 +0200

    Created a utility component to store themes in it.

---
+++ inventorymanagement.client/src/utils/themes.jsx
@@ -0,0 +1,30 @@
+import { createTheme } from '@mui/material/styles';
+
+export const themes = {
+    light: createTheme({
+        palette: {
+            mode: 'light',
+            primary: { main: '#1976d2' },
+            secondary: { main: '#dc004e' },
+            background: {
+                default: '#ffffff',
+            },
+            text: {
+                primary: '#000000',
+            },
+        },
+    }),
+    dark: createTheme({
+        palette: {
+            mode: 'dark',
+            primary: { main: '#90caf9' },
+            secondary: { main: '#f48fb1' },
+            background: {
+                default: '#333333',
+            },
+            text: {
+                primary: '#ffffff',
+            },
+        },
+    })
+};
