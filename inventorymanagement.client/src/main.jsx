54a88d3ceeef9c12cda2d0875c762bbbc5dfbfd9
Emil9411 <almadi.emil94@gmail.com>
2024-08-07 18:45:10 +0200

    New pages registered in the router.

--- inventorymanagement.client/src/main.jsx
+++ inventorymanagement.client/src/main.jsx
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import App from './App.jsx'
 import Login from './pages/Login.jsx'
-import AllItems from './pages/AllItems.jsx'
-import Registration from './pages/Registration.jsx'
 import Verification from './pages/Verification.jsx'
-import Employees from './pages/Employees.jsx'
 import Profile from './pages/Profile.jsx'
-import Inventory from './pages/Inventory.jsx'
+import AllItems from './pages/drawers/items/AllItems.jsx'
+import Consumables from './pages/drawers/items/Consumables.jsx'
+import Equipments from './pages/drawers/items/Equipments.jsx'
+import Ingredients from './pages/drawers/items/Ingredients.jsx'
+import NonConsumables from './pages/drawers/items/NonConsumables.jsx'
+import OrderList from './pages/drawers/items/OrderList.jsx'
+import Products from './pages/drawers/items/Products.jsx'
+import Tools from './pages/drawers/items/Tools.jsx'
+import Registration from './pages/drawers/employees/Registration.jsx'
+import Employees from './pages/drawers/employees/Employees.jsx'
+import RemoveEmployee from './pages/drawers/employees/RemoveEmployee.jsx'
+import Inventory from './pages/drawers/warehouses/Inventory.jsx'
+import AddWarehouse from './pages/drawers/warehouses/AddWarehouse.jsx'
+import Warehouses from './pages/drawers/warehouses/Warehouses.jsx'
+import UpdateWarehouse from './pages/drawers/warehouses/UpdateWarehouse.jsx'
+import DeleteWarehouse from './pages/drawers/warehouses/DeleteWarehouse.jsx'
 import Logout from './components/LogoutButton.jsx'
 import './index.css'
 
@@ -48,6 +60,54 @@ const router = createBrowserRouter([
             {
                 path: "/inventory",
                 element: <Inventory />
+            },
+            {
+                path: "/addwarehouse",
+                element: <AddWarehouse />
+            },
+            {
+                path: "/warehouses",
+                element: <Warehouses />
+            },
+            {
+                path: "/updatewarehouse",
+                element: <UpdateWarehouse />
+            },
+            {
+                path: "/deletewarehouse",
+                element: <DeleteWarehouse />
+            },
+            {
+                path: "/removeemployee",
+                element: <RemoveEmployee />
+            },
+            {
+                path: "/consumables",
+                element: <Consumables />
+            },
+            {
+                path: "/equipments",
+                element: <Equipments />
+            },
+            {
+                path: "/ingredients",
+                element: <Ingredients />
+            },
+            {
+                path: "/nonconsumables",
+                element: <NonConsumables />
+            },
+            {
+                path: "/orderlist",
+                element: <OrderList />
+            },
+            {
+                path: "/products",
+                element: <Products />
+            },
+            {
+                path: "/tools",
+                element: <Tools />
             }
         ]
     },
