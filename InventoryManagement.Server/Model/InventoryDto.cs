80275a390a6c26f708693f95e070c1123c6a977e
Emil9411 <almadi.emil94@gmail.com>
2024-09-04 18:15:34 +0200

    DTOs created for the Item and Inventory classes.

---
+++ InventoryManagement.Server/Model/InventoryDto.cs
@@ -0,0 +1,18 @@
+﻿namespace InventoryManagement.Server.Model
+{
+    public class InventoryDto
+    {
+        public int InventoryId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string? ManagerId { get; set; }
+
+        // List of items related to this inventory, represented by ItemDTO
+        public List<ItemDto> Items { get; set; }
+    }
+}
---
+++ InventoryManagement.Server/Model/ItemDto.cs
@@ -0,0 +1,26 @@
+﻿namespace InventoryManagement.Server.Model
+{
+    public class ItemDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? WeightInGrams { get; set; }
+        public double? WeightInKilograms { get; set; }
+        public int? QuantityInPieces { get; set; }
+        public int? QuantityPerPackage { get; set; }
+        public double? NetPricePerGram { get; set; }
+        public double? NetPricePerKilogram { get; set; }
+        public double? NetPricePerPiece { get; set; }
+        public double? NetPricePerPackage { get; set; }
+        public double? TaxRate { get; set; }
+        public double? GrossPricePerGram { get; set; }
+        public double? GrossPricePerKilogram { get; set; }
+        public double? GrossPricePerPiece { get; set; }
+        public double? GrossPricePerPackage { get; set; }
+        public bool ShouldOrder { get; set; }
+        public bool IsIngredient { get; set; }
+        public bool IsConsumable { get; set; }
+        public bool IsEquipment { get; set; }
+        public int InventoryId { get; set; }
+    }
+}
