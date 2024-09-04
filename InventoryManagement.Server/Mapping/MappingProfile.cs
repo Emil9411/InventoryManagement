663f1d9df9f583e030c3b83c2b43192719530d45
Emil9411 <almadi.emil94@gmail.com>
2024-09-04 18:15:49 +0200

    MappingProfile created.

---
+++ InventoryManagement.Server/Mapping/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using InventoryManagement.Server.Model;
+
+namespace InventoryManagement.Server.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Item, ItemDto>().ReverseMap();
+            CreateMap<Inventory, InventoryDto>().ReverseMap();
+        }
+    }
+}
