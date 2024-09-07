5d70e061a41d3e276ad63469b6a4d040b9f00fda
Emil9411 <almadi.emil94@gmail.com>
2024-09-07 22:13:25 +0200

    Worked out the basic look of the AllItems page.

--- inventorymanagement.client/src/pages/drawers/items/AllItems.jsx
+++ inventorymanagement.client/src/pages/drawers/items/AllItems.jsx
@@ -1,13 +1,15 @@
-import { useState, useEffect } from 'react';
+﻿import { useState, useEffect } from 'react';
 import { Button, Table, Paper, TableRow, TableHead, TableContainer, TableCell, TableBody, Typography } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import LoadingCircle from '../../../components/LoadingCircle';
 import "../../../index.css";
 
 function AllItems() {
     const [items, setItems] = useState([]);
     const [loading, setLoading] = useState(true);
-    
+
 
     async function getItems() {
         try {
@@ -39,7 +41,7 @@ function AllItems() {
                 });
                 const data = await response.json();
                 console.log(data);
-                setItems(data);
+                setItems(data.$values);
             } else {
                 const response = await fetch(`/api/inventory/${user.inventoryId}/items`, {
                     method: 'GET',
@@ -50,7 +52,7 @@ function AllItems() {
                 });
                 const data = await response.json();
                 console.log(data);
-                setItems(data.values);
+                setItems(data.$values);
             }
         } catch (error) {
             console.error(error);
@@ -64,10 +66,44 @@ function AllItems() {
     }, []);
 
     if (loading) {
-        return <LoadingCircle />; 
+        return <LoadingCircle />;
     }
     return (
-        <p>Items</p>
+        <TableContainer component={Paper} sx={{ padding: 6, width: '90vw', marginLeft: 'auto', marginRight: 'auto', marginTop: 6, marginBottom: 6 }}>
+            <Table sx={{ width: '80vw', marginLeft: 'auto', marginRight: 'auto' }} aria-label="employee table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell sx={{ fontWeight: 'bold' }}>Név</TableCell>
+                        <TableCell sx={{ fontWeight: 'bold' }}>Áfa kulcs</TableCell>
+                        <TableCell sx={{ fontWeight: 'bold' }}>Mennyiség (db)</TableCell>
+                        <TableCell sx={{ fontWeight: 'bold' }}>Mennyiség (kg)</TableCell>
+                        <TableCell sx={{ fontWeight: 'bold' }}>Nettó Ár (Ft/db)</TableCell>
+                        <TableCell sx={{ fontWeight: 'bold' }}>Bruttó Ár (Ft/db)</TableCell>
+                        <TableCell sx={{ fontWeight: 'bold' }}>Nettó Ár (Ft/kg)</TableCell>
+                        <TableCell sx={{ fontWeight: 'bold' }}>Bruttó Ár (Ft/kg)</TableCell>
+                        <TableCell sx={{ fontWeight: 'bold' }}>Műveletek</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {items.map((item, index) => (
+                        <TableRow key={index}>
+                            <TableCell>{item.name}</TableCell>
+                            <TableCell>{item.taxRate}</TableCell>
+                            <TableCell>{item.quantityInPieces}</TableCell>
+                            <TableCell>{item.weightInKilograms}</TableCell>
+                            <TableCell>{item.netPricePerPiece}</TableCell>
+                            <TableCell>{item.grossPricePerPiece}</TableCell>
+                            <TableCell>{item.netPricePerKilogram}</TableCell>
+                            <TableCell>{item.grossPricePerKilogram}</TableCell>
+                            <TableCell>
+                                <Button variant="contained" color="primary" sx={{ marginRight: 1 }} startIcon={<EditIcon />}>Szerkesztés</Button>
+                                <Button variant="contained" color="error" startIcon={<DeleteForeverIcon />}>Törlés</Button>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
 }
 
